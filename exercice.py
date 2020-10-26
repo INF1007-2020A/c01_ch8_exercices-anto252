2faa754b6696369932b3c7f59ee0645851769721
anto252 <antoinecc@hotmail.ca>
2020-10-26 14:34:45 -0400

    ACC

--- exercice.py
+++ exercice.py
@@ -2,14 +2,37 @@
 # -*- coding: utf-8 -*-
 
 
-# TODO: Importez vos modules ici
 
 
+def compare(file_name1: str, file_name2: str):
+    with open(file_name1, "r") as A, open(file_name2, "r") as B:
 
-# TODO: Définissez vos fonction ici
+        same = True
+
+        while same:
+            a = A.read()
+            b = B.read()
+
+            if a != b:
+                same = False
+
+    return same
 
 
-if __name__ == '__main__':
-    # TODO: Appelez vos fonctions ici
+def space_extend(file_name):
+    with open(file_name, "r") as A:
+        f = A.read()
 
+        new_text = f.replace(" ", "   ")
+        with open(file_name, "w") as A:
+            A.write(new_text)
+
+    return file_name
+
+def ligne_read():
     pass
+
+
+
+compare("texte1.txt", "texte2.txt")
+space_extend("espace.txt")
